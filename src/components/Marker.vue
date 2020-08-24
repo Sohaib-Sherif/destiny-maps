1067c8fd4a31dc12e98918b725933d985c6dd1f8
Sohaib Sherif <triples17t@gmail.com>
2020-08-24 12:00:44 +0200

    feat: Emit marker object on all marker events

--- src/components/Marker.vue
+++ src/components/Marker.vue
@@ -56,11 +56,11 @@ export default {
           }
         })
       )
-      .then(() => this.marker.addListener('click', (e) => this.$emit('click', e)))
-      .then(() => this.marker.addListener('dblclick', (e) => this.$emit('doubleClick', e)))
-      .then(() => this.marker.addListener('rightclick', (e) => this.$emit('rightClick', e)))
+      .then(() => this.marker.addListener('click', (e) => this.$emit('click', {marker: this.marker, event: e})))
+      .then(() => this.marker.addListener('dblclick', (e) => this.$emit('doubleClick', {marker: this.marker, event: e})))
+      .then(() => this.marker.addListener('rightclick', (e) => this.$emit('rightClick', {marker: this.marker, event: e})))
       .then(() => this.marker.addListener('mouseover', (e) => this.$emit('mouseover', {marker: this.marker, event: e})))
-      .then(() => this.marker.addListener('mouseout', (e) => this.$emit('mouseout', e)))
+      .then(() => this.marker.addListener('mouseout', (e) => this.$emit('mouseout', {marker: this.marker, event: e})))
       .catch((e) => this.handleError(e))
   },
   watch: {
